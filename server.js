51e187e456324064c71e9bf7cd9b92193906fd86
Y-ME-tho <f20240969@pilani.bits-pilani.ac.in>
2025-02-07 23:29:04 +0530

    14

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/puzzlesDB';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb+srv://rishabjha098:zjRHiWgtoqvLnS2m@cluster0.sq6fc.mongodb.net/puzzlesDB?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
